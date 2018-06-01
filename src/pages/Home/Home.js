9c482681dad65ce6929df1cb053ed79199d38321
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-05-31 21:29:16 -0500

    added sections and footer placeholders

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,31 @@ class Home extends Component {
 		return (
 			<div>
 				<div className='container' id='banner'>
+					<div className='container' id='banner-overlay'>
+					</div>
 				</div>
+
+				<section id='benefits'>
+					<h2>The benefits of Trunk Club</h2>
+						<div className='brands'>asd</div>
+						<div className='stylist'>asd</div>
+						<div className='wardrobes'>asd</div>
+						<div className='schedule'>asd</div>
+				</section>
+
+				<section id='shop'>
+					<h2>Three ways to shop</h2>
+						<div className='order'>asd</div>
+						<div className='clubhouse'>asd</div>
+						<div className='custom'>asd</div>						
+				</section>
+
+				<footer>
+					<div id='services'>asd</div>
+					<div id='about'>asd</div>
+					<div id='partners'>asd</div>
+					<div id='icons'>asd</div>
+				</footer>
 			</div>
 		);
 	}
